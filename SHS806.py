c823672782012cda7022f8cf5705b54e5d2fdb4f
Soohan Kim <soohankimdhs@gmail.com>
2020-09-03 23:45:53 -0400

    Added init IDN SCPI command.

--- SHS806.py
+++ SHS806.py
@@ -1,5 +1,10 @@
-import visa
+import pyvisa
 
 class SiglentSHS806():
-    def __init__():
-        print("Hello, World!")
+    def __init__(self):
+        rm = pyvisa.ResourceManager()
+        scope = rm.open_resource('USB0::0xF4EC::0xEE3A::SHS08DAX4R0062::INSTR')
+        print(scope.query("*IDN?"))
+
+if __name__ == '__main__':
+    scope = SiglentSHS806()
